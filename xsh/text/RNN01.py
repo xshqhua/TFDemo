373c554092ab96fe921a5b34b78199650758a152
Xu Shihua <xshqhua@gmail.com>
2017-06-21 14:56:09 +0800

    Test remote kinux suda

---
+++ xsh/text/RNN01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@version: v1.0 
+@author: XSH 
+@license: xshqhua  
+@contact: xshqhua@foxmail.com 
+@site:  
+@software: PyCharm 
+@file: RNN01.py 
+@time: 2017/6/20 12:55 
+"""
+import tensorflow as tf
+sess = tf.Session()
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    print("Test")
+    pass 
